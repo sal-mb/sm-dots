fb64a3b15d39825e3ca38a635cbc85f27d8d509b
sal-mb <samukbore99@gmail.com>
2025-05-01 19:43:40 -0300

    v

--- fresh.sh
+++ fresh.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
-# echo "updating packages"
-# sudo pacman -Syuu --noconfirm
+ echo "updating packages"
+ sudo pacman -Syuu --noconfirm
 
-# # Check if paru is installed
-# if ! command -v paru &> /dev/null; then
-#   echo "paru is not installed. Installing paru..."
-#   sudo pacman -S --noconfirm paru
-# else
-#   echo "paru is already installed."
-# fi
+ # Check if paru is installed
+ if ! command -v paru &> /dev/null; then
+   echo "paru is not installed. Installing paru..."
+   sudo pacman -S --noconfirm paru
+ else
+   echo "paru is already installed."
+ fi
 
-# echo "Installing network tools..."
-# paru -S --noconfirm dialog networkmanager network-manager-applet wireless_tools
+ echo "Installing network tools..."
+read -p "Do you want to install iwd instead of networkmanager? (not recommended) [y/N]: " install_iwd
+if [[ "$install_iwd" == "y" || "$install_iwd" == "Y" ]]; then
+  paru -S --nocofirm iwd impala wireless_tools
+  sudo systemctl disable NetworkManager.service
+  sudo systemctl enable iwd.service
+  sudo mkdir /etc/iwd
+  ln -s ~/sm-dots/main.conf /etc/iwd
+else
+ paru -S --noconfirm networkmanager network-manager-applet wireless_tools
+ sudo systemctl enable NetworkManager.service
+fi
 
-# # Install zoxide, helix, and wezterm using paru
-# echo "Installing terminal tools..."
-# paru -S --noconfirm zoxide helix wezterm yazi lsd fish fisher tealdeer bear 
+ # Install zoxide, helix, and wezterm using paru
+ echo "Installing terminal tools..."
+ paru -S --noconfirm zoxide helix wezterm yazi lsd fish fisher tealdeer bear fd zip
 
-# echo "Installing hyprland packages..."
-# #paru -S --noconfirm hyprland hyprpolkitagent ironbar-git pipewire wireplumber brightnessctl greetd-tuigreet dunst libnotify
+ echo "Installing hyprland packages..."
+ paru -S --noconfirm hyprland hyprpolkitagent ironbar-git pipewire wireplumber brightnessctl greetd-tuigreet dunst libnotify hyprlock hypridle hyprshade-git wlsunset zen-browser
 
-# echo "Installing fonts..."
-# paru -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
+ echo "Installing screenshot tools..."
+ paru -S --noconfirm grim slurp swappy
 
-# echo "Installing gtk themes..."
-# paru -S --noconfirm nwg-look papirus-icon-theme materia-gtk-theme
+ echo "Installing fonts..."
+ paru -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
 
-# echo "Installing qt themes..."
-# paru -S --noconfirm kvantum-qt6-git kvantum-theme-materia
+ echo "Installing gtk themes..."
+ paru -S --noconfirm nwg-look papirus-icon-theme materia-gtk-theme
+
+ echo "Installing qt themes..."
+ paru -S --noconfirm kvantum-qt6-git kvantum-theme-materia qt5-wayland qt6-wayland
 
 # Prompt for laptop power packages installation
-read -p "Do you want to install laptop power packages (powertop and acpi)? [y/n]: " install_laptop_power
+read -p "Do you want to install laptop power packages (tlp and acpi)? [y/N]: " install_laptop_power
 if [[ "$install_laptop_power" == "y" || "$install_laptop_power" == "Y" ]]; then
   echo "Installing laptop power packages..."
-  paru -S --noconfirm powertop acpi
+  paru -S --noconfirm tlp acpi
 
-  echo "Setting powertop service..."
-  read -p "Do you want to calibrate powertop? (takes a long time, your screen may turn black for a while so don't panic) [y/n]: " calibrate_powertop
+  sudo systemctl enable tlp.service
 
-  if [[ "$calibrate_powertop" == "y" || "$install_laptop_power" == "Y" ]]; then
-    echo "Calibrating powertop..."
-    sudo powertop -c
-  fi
-
-  echo "Enabling powertop service"
-  sudo ln -s ~/sm-dots/powertop.service /etc/systemd/system/
-  sudo systemctl enable powertop.service
 else
   echo "Skipping laptop power packages installation."
 fi
@@ -67,5 +71,9 @@ rm -f ~/.wezterm.lua
 ln -s ~/sm-dots/.wezterm.lua ~/.wezterm.lua
 rm -rf ~/.config/ironbar
 ln -snf ~/sm-dots/.config/ironbar ~/.config/ironbar
+rm -rf ~/.config/dunst 
+ln -snf ~/sm-dots/.config/dunst ~/.config/dunst
+rm -rf ~/.config/fuzzel 
+ln -snf ~/sm-dots/.config/fuzzel ~/.config/fuzzel
 sudo rm -rf /etc/greetd/config.toml
 sudo ln -s ~/sm-dots/config.toml /etc/greetd/
